395cb29d8a85f70466bde226d44a00c0fae50615
Raphael Kottakal <raphaelsemail@gmail.com>
2016-06-14 12:41:16 +0530

    Initate and setup blank slate

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # blank-slate
+
+A blank slate to start creating your react page
+
+	npm start - To run the hot loader
+	npm run make - To create production files in dist folder
---
+++ src/components/Page.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import '../css/main.css';
+
+export default class Page extends React.Component {
+
+
+	render() {
+
+		return(
+			<div>
+			
+				Hello world!
+			</div>			
+		);
+
+	}
+
+}
