75c90b673998f7ff35cb8b53d48f83bd60285924
Raphael Kottakal <raphaelsemail@gmail.com>
2016-06-17 18:17:09 +0530

    Complete page

---
+++ src/components/Layering.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import _ from 'lodash';
+
+export default class Layering extends React.Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			loaded: false,
+			one: false,
+			two: false,
+			three: false,
+		};
+
+	}
+
+	componentWillMount() {
+
+		const allImages = _.assignIn({},this.props.imgData.looks,this.props.imgData.switches),
+			totalLength = _.size(allImages),
+			thisClass = this;
+
+		let imagesLoadedArray = new Array;
+
+		_.forEach(allImages, (link, key) => {
+			const image = new Image;
+			image.src = link;
+			image.onload = function() {
+				imagesLoadedArray.push(link);
+				if (imagesLoadedArray.length == totalLength) {
+					thisClass.setState({
+						loaded: true
+					});
+				}
+
+			};
+		});
+
+
+	}
+
+	getLookImg() {
+
+		let key;
+
+		if (this.state.one && !this.state.two && !this.state.three) {
+			key = '01';
+		} else if (!this.state.one && this.state.two && !this.state.three) {
+			key = '02';
+		} else if (!this.state.one && !this.state.two && this.state.three) {
+			key = '03';
+		} else if (this.state.one && this.state.two && !this.state.three) {
+			key = '012';
+		} else if (this.state.one && !this.state.two && this.state.three) {
+			key = '013';
+		} else if (!this.state.one && this.state.two && this.state.three) {
+			key = '023';
+		} else if (this.state.one && this.state.two && this.state.three) {
+			key = '0123';
+		} else {
+			key = '0';
+		}
+
+		return this.props.imgData.looks[key];
+	}
+
+	getSwitchStatus(key) {
+
+		let switchStatus;
+
+		if (this.state[key]) {
+			switchStatus = '-';
+		} else {
+			switchStatus = '+';
+		}
+
+		return switchStatus;
+	}
+
+	handleSwitchClick(key) {
+
+		let stateObj = this.state;
+
+		stateObj[key] = !stateObj[key];
+
+		this.setState(stateObj);
+	}
+
+	render() {
+
+		let styles = {
+			imgs: {
+				display: 'block',
+				width: '100%',
+				height: 'auto'
+			},
+
+			container: {
+				position: 'relative',
+				margin: '16px 0',
+				minHeight: 250,
+			},
+
+			look: {
+				display: 'block',
+				width: '66%'
+			},
+
+			switches: {
+				position: 'absolute',
+				top: 0,
+				right: 0,
+				width: '33%'
+			},
+
+			switch: {
+				position: 'relative'
+			},
+
+			switchStatus: {
+				position: 'absolute',
+				top: 8,
+				left: 8,
+				width: 16,
+				height: 16,
+				lineHeight: '16px',
+				textAlign: 'center',
+				backgroundColor: '#000',
+				color: '#fff',
+				borderRadius: '50%'
+			},
+			loadingOverlay: {
+				position: 'absolute',
+				top: 0,
+				left: 0,
+				width: '100%',
+				height: '100%',
+				backgroundColor: 'hsla(0,0%,0%,0.5)',
+				backgroundImage: 'url(http://assets.myntassets.com/v1466161200/radium/loading.gif)',
+				backgroundRepeat: 'no-repeat',
+				backgroundPosition: 'center center',
+			}
+		}
+
+		return(
+			<div style={styles.container} className="layering-container">
+
+				<img style={styles.look} src={this.getLookImg()} className="layering-look" />
+					
+				<div style={styles.switches} className="layering-switches">
+
+					<div onClick={this.handleSwitchClick.bind(this,'one')} style={styles.switch} className="layering-switch-one">
+						<img style={styles.imgs} src={this.props.imgData.switches[1]} />
+						<div style={styles.switchStatus} className="layering-switch-status">{this.getSwitchStatus('one')}</div>
+					</div>
+					<div onClick={this.handleSwitchClick.bind(this,'two')} style={styles.switch} className="layering-switch-two">
+						<img style={styles.imgs} src={this.props.imgData.switches[2]} />
+						<div style={styles.switchStatus} className="layering-switch-status">{this.getSwitchStatus('two')}</div>
+					</div>
+					<div onClick={this.handleSwitchClick.bind(this,'three')} style={styles.switch} className="layering-switch-three">
+						<img style={styles.imgs} src={this.props.imgData.switches[3]} />
+						<div style={styles.switchStatus} className="layering-switch-status">{this.getSwitchStatus('three')}</div>
+					</div>
+				</div>
+				{ (!this.state.loaded) ? <div style={styles.loadingOverlay}></div>: ''}
+			</div>
+		);
+	}
+
+
+	
+		
+
+}
--- src/components/Page.js
+++ src/components/Page.js
@@ -4,15 +4,60 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import '../css/main.css';
 
+import layeringData from '../data/layered';
+
+import Layering from './Layering';
+
 export default class Page extends React.Component {
 
 
 	render() {
 
+		const css = {
+			mainTitle: {
+				margin: '16px 8px',
+				fontFamily: 'Lora, serif',
+				textAlign: 'center',
+				fontSize: 13,
+				letterSpacing: 2
+			},
+			copy: {
+				margin: '16px 8px',
+				fontSize: 12
+			},
+			shop: {
+				color: 'gray',
+				display: 'block',
+				textAlign: 'center',
+				textDecoration: 'none',
+				margin: '16px 8px',
+				textTransform: 'uppercase',
+				letterSpacing: 2
+			},
+			title: {
+				margin: '16px 8px',
+				paddingTop: 16,
+				fontFamily: 'Lora, serif',
+				textAlign: 'center',
+				fontSize: 13,
+				letterSpacing: 2,
+				borderTop: '1px solid gray',
+			}
+		}
+
 		return(
 			<div>
-			
-				Hello world!
+
+				<div style={css.mainTitle}><span style={{fontSize: 25, textTransform: 'uppercase', letterSpacing: 1}}>College trends</span><br /> you cannot miss this season</div>
+				<div style={css.copy}>Whether it's a jacket or a scarf, small changes make a big difference when it comes to getting ready for college. Make sure you put your best foot forward by incorporating these trendy layering options.</div>
+				
+				<div style={css.title}>Preppy casuals</div>
+				<Layering imgData={layeringData.look1} />
+				<a target="_blank" style={css.shop} href="http://www.myntra.com/layering-denim-jacket-for-men?SRC=Radium">Shop collection</a>
+
+				<div style={css.title}>Colour burst</div>
+				<Layering imgData={layeringData.look2} />
+				<a target="_blank" style={css.shop} href="http://www.myntra.com/layering-red-jacket-for-men?SRC=Radium">Shop collection</a>
 			</div>			
 		);
 
