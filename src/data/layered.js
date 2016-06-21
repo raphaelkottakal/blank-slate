c5c9bf75df3c2ede53ab9c281df79d61d6bfecf6
Raphael Kottakal <raphaelsemail@gmail.com>
2016-06-21 13:37:28 +0530

    update images with new dimentions and align + n - button

--- src/data/layered.js
+++ src/data/layered.js
@@ -1,36 +1,36 @@
 export default {
 	look1: {
 		looks: {
-			'0': 'http://assets.myntassets.com/v1466162526/SIS/college-layering/men-L1-base.jpg',
-			'01': 'http://assets.myntassets.com/v1466162526/SIS/college-layering/men-L1-jacket.jpg',
-			'02': 'http://assets.myntassets.com/v1466162526/SIS/college-layering/men-L1-bag.jpg',
-			'03': 'http://assets.myntassets.com/v1466162527/SIS/college-layering/men-L1-sunglasses.jpg',
-			'012': 'http://assets.myntassets.com/v1466162526/SIS/college-layering/men-L1-jacket-bags.jpg',
-			'013': 'http://assets.myntassets.com/v1466162527/SIS/college-layering/men-L1-jacket-sunglasses.jpg',
-			'023': 'http://assets.myntassets.com/v1466162527/SIS/college-layering/men-L1-sunglasses-bag.jpg',
-			'0123': 'http://assets.myntassets.com/v1466162526/SIS/college-layering/men-L1-all.jpg'
+			'0': 'http://assets.myntassets.com/v1466495506/SIS/college-layering/new/men/men-L1-base.jpg',
+			'01': 'http://assets.myntassets.com/v1466495506/SIS/college-layering/new/men/men-L1-jacket.jpg',
+			'02': 'http://assets.myntassets.com/v1466495506/SIS/college-layering/new/men/men-L1-bag.jpg',
+			'03': 'http://assets.myntassets.com/v1466495507/SIS/college-layering/new/men/men-L1-sunglasses.jpg',
+			'012': 'http://assets.myntassets.com/v1466495506/SIS/college-layering/new/men/men-L1-jacket-bags.jpg',
+			'013': 'http://assets.myntassets.com/v1466495507/SIS/college-layering/new/men/men-L1-jacket-sunglasses.jpg',
+			'023': 'http://assets.myntassets.com/v1466495507/SIS/college-layering/new/men/men-L1-sunglasses-bag.jpg',
+			'0123': 'http://assets.myntassets.com/v1466495506/SIS/college-layering/new/men/men-L1-all.jpg'
 		},
 		switches: {
-			1: 'http://assets.myntassets.com/v1466162525/SIS/college-layering/denim-jacket.jpg',
-			2: 'http://assets.myntassets.com/v1466162526/SIS/college-layering/men-L1--product-bag.jpg',
-			3: 'http://assets.myntassets.com/v1466162526/SIS/college-layering/men-L1--product-sunglasses.jpg'
+			1: 'http://assets.myntassets.com/v1466495506/SIS/college-layering/new/men/denim-jacket.jpg',
+			2: 'http://assets.myntassets.com/v1466495507/SIS/college-layering/new/men/men-L1--product-bag.jpg',
+			3: 'http://assets.myntassets.com/v1466495507/SIS/college-layering/new/men/men-L1--product-sunglasses.jpg'
 		}
 	},
 	look2: {
 		looks: {
-			'0': 'http://assets.myntassets.com/v1466166958/SIS/college-layering/men-L2-base.jpg',
-			'01': 'http://assets.myntassets.com/v1466166958/SIS/college-layering/men-L2-jacket.jpg',
-			'02': 'http://assets.myntassets.com/v1466166958/SIS/college-layering/men-L2-bag.jpg',
-			'03': 'http://assets.myntassets.com/v1466166958/SIS/college-layering/men-L2-cap.jpg',
-			'012': 'http://assets.myntassets.com/v1466166959/SIS/college-layering/men-L2-jacket-bag.jpg',
-			'013': 'http://assets.myntassets.com/v1466166959/SIS/college-layering/men-L2-jacket-cap.jpg',
-			'023': 'http://assets.myntassets.com/v1466166958/SIS/college-layering/men-L2-cap-bag.jpg',
-			'0123': 'http://assets.myntassets.com/v1466166958/SIS/college-layering/men-L2-all.jpg'
+			'0': 'http://assets.myntassets.com/v1466495970/SIS/college-layering/new/men/men-L2-base.jpg',
+			'01': 'http://assets.myntassets.com/v1466495970/SIS/college-layering/new/men/men-L2-jacket.jpg',
+			'02': 'http://assets.myntassets.com/v1466495970/SIS/college-layering/new/men/men-L2-bag.jpg',
+			'03': 'http://assets.myntassets.com/v1466495970/SIS/college-layering/new/men/men-L2-cap.jpg',
+			'012': 'http://assets.myntassets.com/v1466495971/SIS/college-layering/new/men/men-L2-jacket-bag.jpg',
+			'013': 'http://assets.myntassets.com/v1466495971/SIS/college-layering/new/men/men-L2-jacket-cap.jpg',
+			'023': 'http://assets.myntassets.com/v1466495970/SIS/college-layering/new/men/men-L2-cap-bag.jpg',
+			'0123': 'http://assets.myntassets.com/v1466495970/SIS/college-layering/new/men/men-L2-all.jpg'
 		},
 		switches: {
-			1: 'http://assets.myntassets.com/v1466166959/SIS/college-layering/men-L2-product-jacket.jpg',
-			2: 'http://assets.myntassets.com/v1466166959/SIS/college-layering/men-L2-product-bag.jpg',
-			3: 'http://assets.myntassets.com/v1466166959/SIS/college-layering/men-L2-product-cap.jpg'
+			1: 'http://assets.myntassets.com/v1466495971/SIS/college-layering/new/men/men-L2-product-jacket.jpg',
+			2: 'http://assets.myntassets.com/v1466495970/SIS/college-layering/new/men/men-L2-product-bag.jpg',
+			3: 'http://assets.myntassets.com/v1466495970/SIS/college-layering/new/men/men-L2-product-cap.jpg'
 		}
 	},
 }
