54d6b9a05efa70e2ba72491f61fe90002a88fb1f
Jagadesh <csejagadessh@gmail.com>
2016-06-22 17:30:49 +0530

    Update the slider images

--- src/components/Page.js
+++ src/components/Page.js
@@ -12,17 +12,21 @@ export default class Page extends React.Component {
 	render() {
 		var settings = {
 			dots: true,
-			infinite: true,
+			// infinite: true,
 			speed: 500,
-			slidesToShow: 1,
-			slidesToScroll: 1,
+			// slidesToShow: 1,
+			// slidesToScroll: 1,
 			arrows:false,
 			autoplay:true,
-			infinite: true
+			// infinite: true
 			// slide:2
 		};
 		var style={
-			textAlign:'center'
+			textAlign:'center',
+			color: '#494949',
+			padding: '0 30px',
+			fontFamily: 'Source+Sans+Pro',
+			fontSize: '14px'
 		},
 		shopStyle={
 			color:'black',
@@ -36,51 +40,65 @@ export default class Page extends React.Component {
 			width: '115px',
     		fontSize: '14px',
     		letterSpacing: '3px',
-    		marginBottom: '25px'
+    		marginBottom: '25px',
+    		position: 'relative',
+    		top: '-45px'
 		};
 
 		return(
 			<div>
+		        <img src="http://assets.myntassets.com/v1466595169/SIS/beauty-store-eyes/eye-makeup.jpg" style={{ width: '100%', height: 'auto'}}/>
+				
 		        <img src="http://assets.myntassets.com/v1466500057/SIS/beauty-store-eyes/Beauty_store-steps-53-min.jpg" style={{ width: '100%', height: 'auto'}}/>
 				<Slider {...settings}>
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466500056/SIS/beauty-store-eyes/Beauty_store-steps-01-min.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466577756/SIS/beauty-store-eyes/Smokey-eye-step-1-min.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Moisturise your eyelids</p>
 					</div>
 
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466500056/SIS/beauty-store-eyes/Beauty_store-steps-03-min.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466577756/SIS/beauty-store-eyes/Smokey-eye-step-2-min.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Apply primer or foundation</p>
 					</div>
 
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466500056/SIS/beauty-store-eyes/Beauty_store-steps-04-min.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466577756/SIS/beauty-store-eyes/Smokey-eye-step-3-min.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Apply charcoal gray powder just above the crease of the lid</p>
 					</div>
 
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466500056/SIS/beauty-store-eyes/Beauty_store-steps-05-min.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466577756/SIS/beauty-store-eyes/Smokey-eye-step-4-min.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Trace your upper lash line with a black pencil</p>
 					</div>
 
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466500056/SIS/beauty-store-eyes/Beauty_store-steps-06-min.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}>Gently smudge using your fingertip</p>
+		        		<img src="http://assets.myntassets.com/v1466577756/SIS/beauty-store-eyes/Smokey-eye-step-5-min.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<p style={style}>Gently smudge using your fingertips</p>
 					</div>
 
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466500057/SIS/beauty-store-eyes/Beauty_store-steps-07-min.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466577757/SIS/beauty-store-eyes/Smokey-eye-step-6-min.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Sweep gunmetal shade on your lower lash line</p>
 					</div>
 
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466500057/SIS/beauty-store-eyes/Beauty_store-steps-16-min.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466577757/SIS/beauty-store-eyes/Smokey-eye-step-7-min.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Apply silver powder on to the inner corner of the eyes</p>
 					</div>
 
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466500057/SIS/beauty-store-eyes/Beauty_store-steps-15-min.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466577757/SIS/beauty-store-eyes/Smokey-eye-step-8-min.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Apply mascara</p>
+					</div>
+
+					<div>
+		        		<img src="http://assets.myntassets.com/v1466576696/SIS/beauty-store-eyes/Smokey-eye-step-9.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<p style={style}>Wasn’t that easy?</p>
+					</div>
+
+					<div>
+		        		<img src="http://assets.myntassets.com/v1466576696/SIS/beauty-store-eyes/Smokey-eye-step-10.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<p style={style}>You’re all set to paint the town red!</p>
 					</div>
 				</Slider>
 				<a href="http://www.myntra.com/steps-to-ace-smokey-eyes?SRC=Radium" target="_blanck" style={shopStyle}>TRY NOW</a>
@@ -120,11 +138,17 @@ export default class Page extends React.Component {
 		        		<img src="http://assets.myntassets.com/v1466504514/SIS/beauty-store-eyes/Beauty_store-steps-6.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Apply mascara for a dramatic effect</p>
 					</div>
-		        	
-		        	<img src="http://assets.myntassets.com/v1466504515/SIS/beauty-store-eyes/Beauty_store-steps-7.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        	
-		        	<img src="http://assets.myntassets.com/v1466504515/SIS/beauty-store-eyes/Beauty_store-steps-8.jpg" style={{ width: '100%', height: 'auto'}}/>
 					
+					<div>		        	
+		        		<img src="http://assets.myntassets.com/v1466504515/SIS/beauty-store-eyes/Beauty_store-steps-7.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<p style={style}>Voila!</p>
+		        	</div>
+					
+					<div>
+		        		<img src="http://assets.myntassets.com/v1466504515/SIS/beauty-store-eyes/Beauty_store-steps-8.jpg" style={{ width: '100%', height: 'auto'}}/>
+						<p style={style}>You’re all set</p>
+					</div>
+
 				</Slider>
 				<a href="http://www.myntra.com/steps-to-get-electric-blue-eyes?SRC=Radium" target="_blanck" style={shopStyle}>TRY NOW</a>
 
@@ -142,27 +166,27 @@ export default class Page extends React.Component {
 
 					<div>
 		        		<img src="http://assets.myntassets.com/v1466505190/SIS/beauty-store-eyes/Beauty_store-steps-17-min.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}>Put on some eyeshadow in a shade closest to your skin tone</p>
+		        		<p style={style}>Follow up with some primer</p>
 					</div>
 					
 					<div>
 		        		<img src="http://assets.myntassets.com/v1466505190/SIS/beauty-store-eyes/Beauty_store-steps-19-min.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}>Take your eyeliner and make a tiny dot at the corner of each eyelid</p>
+		        		<p style={style}>Put on some eyeshadow in a shade closest to your skin tone</p>
 					</div>
 					
 					<div>
 		        		<img src="http://assets.myntassets.com/v1466505189/SIS/beauty-store-eyes/Beauty_store-steps-20-min.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}>Draw a thin line connecting this dot the end of your eyelid</p>
+		        		<p style={style}>Take your eyeliner and make a tiny dot at the corner of each eyelid</p>
 					</div>
 					
 					<div>
 		        		<img src="http://assets.myntassets.com/v1466505190/SIS/beauty-store-eyes/Beauty_store-steps-21-min.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}>Draw another line to connect the dot to the middle of your eyelid</p>
+		        		<p style={style}>Draw a thin line connecting this dot the end of your eyelid</p>
 					</div>
 					
 					<div>
 		        		<img src="http://assets.myntassets.com/v1466505190/SIS/beauty-store-eyes/Beauty_store-steps-22-min.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}>Fill in this outline, making it as thick as you wish</p>
+		        		<p style={style}>Draw another line to connect the dot to the middle of your eyelid</p>
 					</div>
 					
 					<div>		        	
@@ -172,17 +196,17 @@ export default class Page extends React.Component {
 					
 					<div>		        	
 		        		<img src="http://assets.myntassets.com/v1466505191/SIS/beauty-store-eyes/Beauty_store-steps-24-min.jpg" style={{ width: '100%', height: 'auto'}}/>
-						<p style={style}>Fill in this outline, making it as thick as you wish</p>
+						<p style={style}>Clean up any smudges using an earbud dipped in make-up remover</p>
 					</div>
 
 					<div>		        	
 		        		<img src="http://assets.myntassets.com/v1466505192/SIS/beauty-store-eyes/Beauty_store-steps-25-min.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}>Fill in this outline, making it as thick as you wish</p>
+		        		<p style={style}>Finish off with mascara for a dramatic effect</p>
 					</div>		        	
 					
 					<div>		        	
 		        		<img src="http://assets.myntassets.com/v1466505192/SIS/beauty-store-eyes/Beauty_store-steps-26-min.jpg" style={{ width: '100%', height: 'auto'}}/>
-						<p style={style}>Fill in this outline, making it as thick as you wish</p>
+						<p style={style}>Ta-da!</p>
 					</div>
 
 
@@ -196,53 +220,58 @@ export default class Page extends React.Component {
 				<img src="http://assets.myntassets.com/v1466506228/SIS/beauty-store-eyes/Beauty-store-steps-simple-day-title.jpg" style={{ width: '100%', height: 'auto'}}/>
 				<Slider {...settings}>
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506362/SIS/beauty-store-eyes/Beauty_store-simple-day-step-1.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466594239/SIS/beauty-store-eyes/simple-day-look-1.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Moisturise your eyelids</p>
 					</div>
 
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506362/SIS/beauty-store-eyes/Beauty_store-simple-day-step-2.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466594239/SIS/beauty-store-eyes/simple-day-look-2.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Apply primer or foundation</p>
 					</div>
 					
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506362/SIS/beauty-store-eyes/Beauty_store-simple-day-step-3.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466594239/SIS/beauty-store-eyes/simple-day-look-3.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Apply a matte eyeshadow in an earthy tone along the crease of your eyelid</p>
 					</div>
 					
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506364/SIS/beauty-store-eyes/Beauty_store-simple-day-step-4.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466594239/SIS/beauty-store-eyes/simple-day-look-4.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Take a brown eyeliner pencil and draw a smooth line, as close to your eyelash line as possible</p>
 					</div>
 					
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506365/SIS/beauty-store-eyes/Beauty_store-simple-day-step-5.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466594239/SIS/beauty-store-eyes/simple-day-look-5.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Follow this by applying a lighter shade of the same eyeshadow all over your eyelid</p>
 					</div>
 					
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506363/SIS/beauty-store-eyes/Beauty_store-simple-day-step-6.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<img src="http://assets.myntassets.com/v1466594239/SIS/beauty-store-eyes/simple-day-look-6.jpg" style={{ width: '100%', height: 'auto'}}/>
 		        		<p style={style}>Apply mascara </p>
 					</div>
 					
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506363/SIS/beauty-store-eyes/Beauty_store-simple-day-step-7.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}>Use a light eye pencil to line your lower lids and make your eyes look bigger and brighter. White is for people with fair skin while dull gold works for those who are slightly dusky.</p>
+		        		<img src="http://assets.myntassets.com/v1466594240/SIS/beauty-store-eyes/simple-day-look-7.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<p style={style}>Use a light eye pencil to line your lower lids and make your eyes look bigger and brighter.</p>
 					</div>
 					
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506363/SIS/beauty-store-eyes/Beauty_store-simple-day-step-8.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}> Finish by adding a touch of highlighter below your brow bone to give it more definition</p>
+		        		<img src="http://assets.myntassets.com/v1466594240/SIS/beauty-store-eyes/simple-day-look-8.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<p style={style}>Those with fairer skin can go for white, while dull gold works for those who are slightly dusky.</p>
 					</div>
 					
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506364/SIS/beauty-store-eyes/Beauty_store-simple-day-step-9.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}> </p>
+		        		<img src="http://assets.myntassets.com/v1466594240/SIS/beauty-store-eyes/simple-day-look-9.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<p style={style}>Finish by adding a touch of highlighter below your brow bone to give it more definition</p>
 					</div>
 					
 					<div>
-		        		<img src="http://assets.myntassets.com/v1466506365/SIS/beauty-store-eyes/Beauty_store-simple-day-step-10.jpg" style={{ width: '100%', height: 'auto'}}/>
-		        		<p style={style}> </p>
+		        		<img src="http://assets.myntassets.com/v1466594240/SIS/beauty-store-eyes/simple-day-look-10.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<p style={style}>And that’s all</p>
+					</div>
+					
+					<div>
+		        		<img src="http://assets.myntassets.com/v1466594240/SIS/beauty-store-eyes/simple-day-look-11.jpg" style={{ width: '100%', height: 'auto'}}/>
+		        		<p style={style}>Don’t you look pretty!</p>
 					</div>
 				</Slider>
 				<a href="http://www.myntra.com/steps-to-master-simple-day-look?SRC=Radium" target="_blanck" style={shopStyle}>TRY NOW</a>
