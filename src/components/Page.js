60550b526ca2fb71cb410e0a149089b9c93c6405
Raphael Kottakal <raphaelsemail@gmail.com>
2016-06-24 17:26:59 +0530

    test component

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,15 +4,54 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import '../css/main.css';
 
+import {VelocityComponent, VelocityTransitionGroup, velocityHelpers} from 'velocity-react';
+// import 'velocity-animate/velocity.ui';
+
 export default class Page extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			one: true,
+			two: true
+		};
+	}
+
+	handleToggle(key) {
+
+		switch (key) {
+			case 'one':
+				this.setState({
+					one: !this.state.one
+				});
+				break;
+			case 'two':
+				this.setState({
+					two: !this.state.two
+				});
+				break;
+		}
+		
+	}
 
 
 	render() {
 
+		const animation = {
+			color: this.state.one ? '#32CD32' : '#FF6347'
+		}
+
 		return(
 			<div>
-			
-				Hello world!
+				<div>
+					
+				</div>
+				<VelocityComponent animation={animation} duration={500}>
+					<div>
+						<div>Hello world!</div>
+					</div>
+				</VelocityComponent>
+				<button onClick={this.handleToggle.bind(this,'one')}>Toggle</button>
 			</div>			
 		);
 
