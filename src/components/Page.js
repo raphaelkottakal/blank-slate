37487ca3e5df8c0d2316e5d7e14f14a891c3255e
Raphael Kottakal <raphaelsemail@gmail.com>
2016-06-24 15:33:50 +0530

    update design fixes

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import '../css/main.css';
 
+import _ from 'lodash';
+
 import layeringData from '../data/layered';
 
 import Layering from './Layering';
@@ -14,36 +16,50 @@ export default class Page extends React.Component {
 
 		const css = {
 			mainTitle: {
-				margin: '16px 8px',
+				margin: '10px 8px',
 				fontFamily: 'Lora, serif',
 				textAlign: 'center',
-				fontSize: 13,
-				letterSpacing: 2
+				fontSize: 10,
+				letterSpacing: 2,
+				textTransform: 'uppercase',
 			},
 			copy: {
-				margin: '16px 8px',
-				fontSize: 12
+				margin: '10px 8px',
+				fontSize: 13,
+				textAlign: 'center',
+				padding: '0 16px',
+				lineHeight: '17px'
 			},
 			shop: {
-				color: 'gray',
+				color: 'black',
 				display: 'block',
 				textAlign: 'center',
 				textDecoration: 'none',
-				margin: '16px 8px',
+				margin: '16px auto',
 				textTransform: 'uppercase',
-				letterSpacing: 2
+				// letterSpacing: 2,
+				backgroundColor: '#98E3E2',
+				padding: '4px',
+				width: 100,
+				fontSize: 13
+
 			},
 			title: {
 				margin: '16px 8px',
-				paddingTop: 16,
+				textTransform: 'uppercase',
+				// paddingTop: 16,
 				fontFamily: 'Lora, serif',
 				textAlign: 'center',
-				fontSize: 13,
+				// fontSize: 13,
 				letterSpacing: 2,
-				borderTop: '1px solid gray',
+				// borderTop: '1px solid gray',
 			},
 			cta: {
-				textAlign: 'center'
+				fontSize: 13,
+				paddingTop: 16,
+				color: 'gray',
+				textAlign: 'center',
+				fontStyle: 'italic'
 			}
 		}
 
@@ -51,16 +67,18 @@ export default class Page extends React.Component {
 			<div>
 
 				<div style={css.mainTitle}><span style={{fontSize: 25, textTransform: 'uppercase', letterSpacing: 1}}>College trends</span><br /> you cannot miss this season</div>
+				<hr width="60%" />
 				<div style={css.copy}>Whether it's a jacket or a scarf, small changes make a big difference when it comes to getting ready for college. Make sure you put your best foot forward by incorporating these trendy layering options.</div>
 				<div style={css.cta}>Click to add or remove layers</div>
 
-				<div style={css.title}>Preppy casuals</div>
 				<Layering imgData={layeringData.look1} />
+				<div style={css.title}>Preppy casuals</div>
 				<a target="_blank" style={css.shop} href="http://www.myntra.com/preppy-casuals?SRC=Radium">Shop look</a>
 
-				<div style={css.title}>Colour burst</div>
+				<div style={{borderBottom: '1px solid gray', width: '60%', margin: '32px auto' }} />
 				<Layering imgData={layeringData.look2} />
-				<a target="_blank" style={css.shop} href="http://www.myntra.com/colour-burst?SRC=Radium">Shop look</a>
+				<div style={css.title}>Colour burst</div>
+				<a target="_blank" style={_.assign({},css.shop,{backgroundColor: '#FFD782'})} href="http://www.myntra.com/colour-burst?SRC=Radium">Shop look</a>
 			</div>			
 		);
 
