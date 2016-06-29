3ddf405ee136e8915d36e1476c7336e68edcefd5
Raphael Kottakal <raphaelsemail@gmail.com>
2016-06-29 18:25:50 +0530

    add sample carousel, componets and packages

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import '../css/main.css';
 
+import Carousel from './carousel';
+
 export default class Page extends React.Component {
 
 
@@ -12,7 +14,12 @@ export default class Page extends React.Component {
 		return(
 			<div>
 			
-				Hello world!
+				<Carousel initialSlideHeight={400}>
+					<img src="http://placehold.it/360x400/ffffff/c0392b/&text=slide1" />
+					<img src="http://placehold.it/360x400/ffffff/c0392b/&text=slide2" />
+					<img src="http://placehold.it/360x400/ffffff/c0392b/&text=slide3" />
+					<img src="http://placehold.it/360x400/ffffff/c0392b/&text=slide4" />
+				</Carousel>
 			</div>			
 		);
 
