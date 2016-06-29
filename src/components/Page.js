e347619a58b4ca4033c6e75ba0b102562ba9a420
Raphael Kottakal <raphaelsemail@gmail.com>
2016-06-29 17:53:35 +0530

    add analytics.js and test events

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,13 +6,36 @@ import '../css/main.css';
 
 export default class Page extends React.Component {
 
+	handleClickShoes(event) {
+		ga('send', 'event', 'Radium', 'testPage', event.target.href);
+	}
+
+	handleClickPants(event) {
+		ga('send', 'event', 'Radium', 'testPage', event.target.href);
+	}
+
 
 	render() {
+
+		const css = {
+			shop: {
+				display: 'block',
+				backgroundColor: 'tomato',
+				textAlign: 'center',
+				marginTop: 16,
+				padding: 4,
+				color: 'white',
+				textDecoration: 'none'
+			}
+		}
 
 		return(
 			<div>
 			
 				Hello world!
+
+				<a style={css.shop} href="http://www.myntra.com/shoes" target="_blank" onClick={this.handleClickShoes}>Shoes</a>
+				<a style={css.shop} href="http://www.myntra.com/pants" target="_blank" onClick={this.handleClickPants}>Pants</a>
 			</div>			
 		);
 
