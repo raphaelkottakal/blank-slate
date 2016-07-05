3a3c5abe322e1cd832cf271df65da43b406fe275
Raphael Kottakal <raphaelsemail@gmail.com>
2016-07-05 17:06:59 +0530

    change image and add ga

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,7 +6,13 @@ import _ from 'lodash';
 import 'normalize.css/normalize.css';
 import '../css/main.css';
 
+import Ga from '../functions/Ga';
+
 export default class Page extends React.Component {
+
+	handelShopBtnClick(e) {
+		Ga({label: e.target.href});
+	}
 
 
 	render() {
@@ -44,28 +50,28 @@ export default class Page extends React.Component {
 		return(
 			<div>
 
-				<img style={css.img} src="http://assets.myntassets.com/v1466662124/makeup-trends_ia1sed.jpg" />
+				<img style={css.img} src="http://assets.myntassets.com/v1467717482/radium/SIS/makeup-trends-to-watch-out-for.jpg" />
 				<div style={{margin: '4px 1% 16px 1%'}}>Want to know the season’s hottest beauty trends? We're giving you the low down.</div>
 
 			
 				<img style={css.img} src="http://assets.myntassets.com/v1466499961/SIS/beauty-makeup/candy-apple-lips-min.jpg" />
 				<div style={css.text}>This irresistible shade of red is all the rage right now. P.S. - It’s perfect for date night</div>
-				<a style={_.assignIn({},css.shop,{color: '#F5444C'})} href="http://www.myntra.com/candy-apple-lips?SRC=Radium" target="_blank">Try now</a>
+				<a onClick={this.handelShopBtnClick} style={_.assignIn({},css.shop,{color: '#F5444C'})} href="http://www.myntra.com/candy-apple-lips?SRC=Radium" target="_blank">Try now</a>
 				<div style={_.assignIn({}, css.hr, {borderColor: '#F5444C'})} />
 
 				<img style={css.img} src="http://assets.myntassets.com/v1466499961/SIS/beauty-makeup/shimmer-eyeshadow-min.jpg" />
 				<div style={css.text}>A glitter eyeshadow is like wearing jewellery on your face. This make-up trend is sure to make you sparkle.</div>
-				<a style={_.assignIn({},css.shop,{color: '#4E4131'})} href="http://www.myntra.com/shimmer-eyeshadows?SRC=Radium" target="_blank">Try now</a>
+				<a onClick={this.handelShopBtnClick} style={_.assignIn({},css.shop,{color: '#4E4131'})} href="http://www.myntra.com/shimmer-eyeshadows?SRC=Radium" target="_blank">Try now</a>
 				<div style={_.assignIn({},css.hr, {borderColor: '#4E4131'})} />
 
 				<img style={css.img} src="http://assets.myntassets.com/v1466499961/SIS/beauty-makeup/electric-blue-eyes-min.jpg" />
 				<div style={css.text}>Ditch the pastels and go bold with electric blue eyes. Stock up on everything blue from eyeshadows to eye pencils.</div>
-				<a style={_.assignIn({},css.shop,{color: '#007EF3'})} href="http://www.myntra.com/electric-blue-eyes?SRC=Radium" target="_blank">Try now</a>
+				<a onClick={this.handelShopBtnClick} style={_.assignIn({},css.shop,{color: '#007EF3'})} href="http://www.myntra.com/electric-blue-eyes?SRC=Radium" target="_blank">Try now</a>
 				<div style={_.assignIn({},css.hr, {borderColor: '#007EF3'})} />
 
 				<img style={css.img} src="http://assets.myntassets.com/v1466499961/SIS/beauty-makeup/sun-kissed-bronzer-min.jpg" />
 				<div style={css.text}>Fake that perfect tan with a the season’s must-have vanity item - the sun-kissed bronzer!</div>
-				<a style={_.assignIn({},css.shop,{color: '#4E4131'})} href="http://www.myntra.com/bronzer?SRC=Radium" target="_blank">Try now</a>
+				<a onClick={this.handelShopBtnClick} style={_.assignIn({},css.shop,{color: '#4E4131'})} href="http://www.myntra.com/bronzer?SRC=Radium" target="_blank">Try now</a>
 				<div style={_.assignIn({},css.hr, {borderColor: '#4E4131'})} />
 			</div>			
 		);
