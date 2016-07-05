04cbcb32b9a5849f3e107f43f1185eb04b4a19cc
Raphael Kottakal <raphaelsemail@gmail.com>
2016-07-05 16:42:20 +0530

    create ga fuction

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 
 A blank slate to start creating your react page
 
+	npm i - To install dependencies
 	npm start - To run the hot loader
 	npm run make - To create production files in dist folder
+
+Google analytics function
+	import into component using '../functions/Ga'
+	pass Object into function with 'label'. required.
+	Optionally you can overwrite the category name and action, which defaults to 'Radium' and current page url respectivly.
+	Note: if current page url contains 'localhost', the data will not be sent to ga but will be logged in the console.
--- src/components/Page.js
+++ src/components/Page.js
@@ -4,38 +4,16 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import '../css/main.css';
 
+import Ga from '../functions/Ga';
+
 export default class Page extends React.Component {
 
-	handleClickShoes(event) {
-		ga('send', 'event', 'Radium', window.location.href, event.target.href);
-	}
-
-	handleClickPants(event) {
-		ga('send', 'event', 'Radium', window.location.href, event.target.href);
-	}
-
-
 	render() {
-
-		const css = {
-			shop: {
-				display: 'block',
-				backgroundColor: 'tomato',
-				textAlign: 'center',
-				marginTop: 16,
-				padding: 4,
-				color: 'white',
-				textDecoration: 'none'
-			}
-		}
 
 		return(
 			<div>
 			
 				Hello world!
-
-				<a style={css.shop} href="http://www.myntra.com/shoes" target="_blank" onClick={this.handleClickShoes}>Shoes</a>
-				<a style={css.shop} href="http://www.myntra.com/pants" target="_blank" onClick={this.handleClickPants}>Pants</a>
 			</div>			
 		);
 
