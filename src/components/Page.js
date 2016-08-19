4a45451e54d08656da5621e19b70d22ab412d653
Raphael Kottakal <raphaelsemail@gmail.com>
2016-08-19 19:39:59 +0530

    finish men page

--- src/components/Page.js
+++ src/components/Page.js
@@ -5,11 +5,34 @@ import GifSlider from './gifslider';
 import 'normalize.css/normalize.css';
 import '../css/main.css';
 
+import _ from 'lodash';
+
 import Ga from '../functions/Ga';
 
 export default class Page extends React.Component {
 
 	render() {
+
+		const imgs = {
+			skinny: [
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471612915396-Myntra-Joy-Datta1553-A.jpg",
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471612915385-Myntra-Joy-Datta1557-A.jpg",
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471612915374-Myntra-Joy-Datta1584-A.jpg",
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471612915364-Myntra-Joy-Datta1605-A.jpg",
+			],
+			slim: [
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471614339785-Myntra-Joy-Datta1662-A.jpg",
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471614339809-Myntra-Joy-Datta1657-A.jpg",
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471614339836-Myntra-Joy-Datta1648-A.jpg",
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471614339853-Myntra-Joy-Datta1644-A.jpg",
+			],
+			straight: [
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471614428907-Myntra-Joy-Datta1620-A.jpg",
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471614428892-Myntra-Joy-Datta1633-A.jpg",
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471614428877-Myntra-Joy-Datta1639-A.jpg",
+				"http://assets.myntassets.com/assets/images/lookbook/denim-men/2016/8/19/11471614428923-Myntra-Joy-Datta1618-A.jpg",
+			]
+		}
 
 		const images = [
 			"http://assets.myntassets.com/assets/images/lookbook/2016/8/19/11471602640860-myntra-joy-datta-23730-A.jpg",
@@ -19,59 +42,134 @@ export default class Page extends React.Component {
 		]
 
 		const css = {
+			mainTitleOne: {
+				fontFamily: "'Playfair Display', serif",
+				textAlign: 'center',
+				fontSize: 28,
+				color: '#0A206F',
+				marginTop: 8,
+			},
+			mainTitleTwo: {
+				textAlign: 'center',
+				fontSize: 50,
+				color: '#0A206F',
+				marginBottom: 8,
+				fontFamily: "Didot, serif",
+				textTransform: 'uppercase',
+				lineHeight: '45px',
+			},
 			imageHalf: {
+				// paddingLeft: 4,
+				// paddingRight: 4,
+				// paddingTop: 0,
+				paddingBottom: 32,
 				float: 'left',
-				// display: 'inline-block',
-				width: '60%'
+				width: '62%'
 			},
 			textHalf: {
+				padding: 4,
 				float: 'left',
-				// display: 'inline-block',
-				width: '40%'
+				width: '38%'
 			},
 			number: {
+				fontWeight: 'lighter',
 				fontSize: 64,
 				color: '#F4F4F4',
 				marginBottom: -16,
 			},
 			title: {
-				fontSize: 32,
+				fontSize: 26,
 				fontWeight: 'bold',
 				color: '#5A5B5B',
-				textTransform: 'uppercase'
+				textTransform: 'uppercase',
+				lineHeight: '25px',
 			},
 			hr: {
 				width: 32,
 				borderColor: '#5A5B5B',
-				margin: 0
+				margin: 0,
 			},
 			subTitle: {
+				fontSize: 13,
 				color: '#000',
 				textTransform: 'uppercase',
 				fontWeight: 'bold',
-				marginTop: 8
+				marginTop: 8,
+				letterSpacing: 1,
 			},
 			content: {
-				marginTop: 16
+				fontSize: 14,
+				fontFamily: 'Whitney, san-serif',
+				marginTop: 8,
+				color: '#828280'
+			},
+			shop: {
+				display: 'inline-block',
+				textDecoration: 'none',
+				padding: 4,
+				marginTop: 13,
+				textTransform: 'uppercase',
+				fontSize: 12,
+				color: '#000',
+				backgroundColor: '#efefef',
+				letterSpacing: 2
 			}
 		}
 
 		return(
 			<div>
 
-				<div>
+				<div style={css.mainTitleOne}>find your perfect</div>
+				<div style={css.mainTitleTwo}>Pair</div>
+
+				<div className="group">
 					<div style={css.imageHalf}>
 						<GifSlider 
-							imgs = {images}
+							imgs = {imgs.skinny}
 							timer = {1500}
 						/>
 					</div>
-					<div style={css.textHalf}>
+					<div style={_.assignIn({}, css.textHalf, {paddingLeft: 8})}>
 						<div style={css.number}>01</div>
-						<div style={css.title}>Super<br/>skinny</div>
+						<div style={css.title}>Skinny</div>
 						<hr style={css.hr} />
-						<div style={css.subTitle}>The skin fit</div>
-						<div style={css.content}>Donec dolor tortor, ullamcorper eu imperdiet sed, ultrices quis ex.</div>
+						<div style={css.subTitle}>The snug fit</div>
+						<div style={css.content}>Usually fits well around your waist, hips and legs and tapers down towards the ankles.</div>
+						<a style={css.shop} href="http://www.myntra.com/men-jeans-store?f=Fit_article_attr%3Askinny%2520fit%2Csuper%2520skinny%2520fit" target="_blank">Shop now</a>
+					</div>
+				</div>
+
+				<div className="group">
+					<div style={_.assignIn({},css.textHalf,{textAlign: 'right', paddingRight: 8})}>
+						<div style={css.number}>02</div>
+						<div style={css.title}>Slim</div>
+						<hr style={_.assignIn({},css.hr,{margin: '0 0 0 auto'})} />
+						<div style={css.subTitle}>The narrow fit</div>
+						<div style={css.content}>These normally fit well around your hips and waist while the leg is slightly narrower than straight jeans and slightly broader than skinny jeans.</div>
+						<a style={css.shop} href="http://www.myntra.com/men-jeans-store?f=Fit_article_attr%3Aslim%2520fit%2Ctapered" target="_blank">Shop now</a>
+					</div>
+					<div style={css.imageHalf}>
+						<GifSlider 
+							imgs = {imgs.slim}
+							timer = {1500}
+						/>
+					</div>
+				</div>
+
+				<div className="group">
+					<div style={css.imageHalf}>
+						<GifSlider 
+							imgs = {imgs.straight}
+							timer = {1500}
+						/>
+					</div>
+					<div style={_.assignIn({}, css.textHalf, {paddingLeft: 8})}>
+						<div style={css.number}>03</div>
+						<div style={_.assignIn( {}, css.title, {fontSize: 20})}>Straight</div>
+						<hr style={css.hr} />
+						<div style={css.subTitle}>The comfy fit</div>
+						<div style={css.content}>This good old classic style is the most comfortable and the width of the leg is proportionate between your knees and ankles.</div>
+						<a style={css.shop} href="http://www.myntra.com/men-jeans-store?f=Fit_article_attr%3Aregular%2520fit" target="_blank">Shop now</a>
 					</div>
 				</div>
 				
